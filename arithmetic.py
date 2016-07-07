d9104cf9aba89c0afeffbabf8791960328023008
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 11:56:20 -0700

    Fixed the multiply and the division functions

--- arithmetic.py
+++ arithmetic.py
@@ -5,10 +5,10 @@ def subtract(num1, num2):
     return num1 - num2
 
 def multiply(num1, num2):
-    pass
+    return num1 * num2
 
 def divide(num1, num2):
-    pass
+    return num1/num2
 
 def square(num1):
     pass
